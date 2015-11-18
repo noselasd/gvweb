bd82d6f68579c482954b478ed9added37e3fa968
Nils O. Selåsdal <noselasd@fiane.dyndns.org>
2015-11-18 18:06:20 +0100

    extract http handlers from main to handlers.go

---
+++ handers.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var g_supportedTools = [...]string{"dot", "neato", "twopi", "circo", "fdp", "sfdp", "patchwork"}
+
+func homePage(w http.ResponseWriter, r *http.Request, matches []string) {
+	http.ServeFile(w, r, "static/index.html")
+}
+
+func isSupportedTool(tool string) bool {
+	for _, val := range g_supportedTools {
+		if tool == val {
+			return true
+		}
+	}
+	return false
+}
+
+func generateHandler(w http.ResponseWriter, r *http.Request, matches []string) {
+	graph := r.FormValue("graphtext")
+	imgType := r.FormValue("imagetype")
+	tool := r.FormValue("tool")
+
+	if !isSupportedTool(tool) {
+		http.Error(w, fmt.Sprintf("Tool '%s' is not supported", tool), http.StatusBadRequest)
+		return
+	}
+	if len(imgType) == 0 {
+		http.Error(w, fmt.Sprintf("imagetype is not specified", tool), http.StatusBadRequest)
+		return
+	}
+
+	if len(graph) == 0 {
+		http.Error(w, "Empty input", http.StatusBadRequest)
+		return
+	}
+
+	result := runGraphviz(tool, graph, imgType)
+	if result.err != nil {
+		log.Print(result.err)
+		http.Error(w, result.err.Error(), http.StatusNotAcceptable)
+		return
+	} else {
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(result.fileName))
+		return
+	}
+}
--- main.go
+++ main.go
@@ -11,54 +11,6 @@ import (
 	"time"
 )
 
-const g_DataDir = "data/"
-
-var g_supportedTools = [...]string{"dot", "neato", "twopi", "circo", "fdp", "sfdp", "patchwork"}
-
-func homePage(w http.ResponseWriter, r *http.Request, matches []string) {
-	http.ServeFile(w, r, "static/index.html")
-}
-
-func isSupportedTool(tool string) bool {
-	for _, val := range g_supportedTools {
-		if tool == val {
-			return true
-		}
-	}
-	return false
-}
-
-func generateHandler(w http.ResponseWriter, r *http.Request, matches []string) {
-	graph := r.FormValue("graphtext")
-	imgType := r.FormValue("imagetype")
-	tool := r.FormValue("tool")
-
-	if !isSupportedTool(tool) {
-		http.Error(w, fmt.Sprintf("Tool '%s' is not supported", tool), http.StatusBadRequest)
-		return
-	}
-	if len(imgType) == 0 {
-		http.Error(w, fmt.Sprintf("imagetype is not specified", tool), http.StatusBadRequest)
-		return
-	}
-
-	if len(graph) == 0 {
-		http.Error(w, "Empty input", http.StatusBadRequest)
-		return
-	}
-
-	result := runGraphviz(tool, graph, imgType)
-	if result.err != nil {
-		log.Print(result.err)
-		http.Error(w, result.err.Error(), http.StatusNotAcceptable)
-		return
-	} else {
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(result.fileName))
-		return
-	}
-}
-
 func accessLogger(l chan string) {
 	for {
 		log.Print(<-l)
@@ -121,6 +73,8 @@ var g_TLSCert = flag.String("tlscert", "tls.cert", "Path to TLS certificate file
 var g_TLSKey = flag.String("tlskey", "tls.key", "Path to TLS key file")
 var g_CleanupInterval = flag.Int("purge", 24*60*60, "Remove saved graphs that are older than this amount in seconds. 0 to keep them forever.")
 var g_Version = "DEVELOPMENT"
+
+const g_DataDir = "data/"
 
 func main() {
 	flag.Usage = func() {
